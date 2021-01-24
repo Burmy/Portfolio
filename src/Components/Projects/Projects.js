486828a96abb47f6cc82dcb7055b13ca843c3688
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-23 21:09:00 -0800

    improved project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,38 +4,120 @@ import './Projects.css'
 const Projects = () => {
     return (
         <div className="center projects">
-            <div className="projects-box">
+            <div className="project-box">
 
                 <h1 className="center mp">My Projects</h1>
 
-                <div className="center card ">
-                    <div className="center image">
-                        image here
-                     </div>
-                    <div className="center data">
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img"></div>
+                    </div>
+                    <div className="center project-data">
                         <h1>Wildfire Tracker</h1>
                         <p>This website tracks the wildfires going around the world using a NASA API.</p>
-                        <div className="skills">
+                        <div className="project-skills">
                             <p className="skill">HTML</p>
                             <p className="skill">CSS</p>
                             <p className="skill">JavaScript</p>
                             <p className="skill">ReactJs</p>
                             <p className="skill">NASA API</p>
                         </div>
+                        <div className="project-links">
+                            <p className="demo">Live Demo</p>
+                            <p className="source">View Source</p>
+
+                        </div>
                     </div>
                 </div>
 
-                <div className="center card ">
-                    <div className="image">
-                        image here
-                     </div>
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img"></div>
+                    </div>
+                    <div className="center project-data">
+                        <h1>Wildfire Tracker</h1>
+                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
+                        <div className="project-skills">
+                            <p className="skill">HTML</p>
+                            <p className="skill">CSS</p>
+                            <p className="skill">JavaScript</p>
+                            <p className="skill">ReactJs</p>
+                            <p className="skill">NASA API</p>
+                        </div>
+                        <div className="project-links">
+                            <p className="demo">Live Demo</p>
+                            <p className="source">View Source</p>
+
+                        </div>
+                    </div>
                 </div>
 
-                <div className="center card ">
-                    <div className="image">
-                        image here
-                     </div>
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img"></div>
+                    </div>
+                    <div className="center project-data">
+                        <h1>Wildfire Tracker</h1>
+                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
+                        <div className="project-skills">
+                            <p className="skill">HTML</p>
+                            <p className="skill">CSS</p>
+                            <p className="skill">JavaScript</p>
+                            <p className="skill">ReactJs</p>
+                            <p className="skill">NASA API</p>
+                        </div>
+                        <div className="project-links">
+                            <p className="demo">Live Demo</p>
+                            <p className="source">View Source</p>
+
+                        </div>
+                    </div>
                 </div>
+
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img"></div>
+                    </div>
+                    <div className="center project-data">
+                        <h1>Wildfire Tracker</h1>
+                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
+                        <div className="project-skills">
+                            <p className="skill">HTML</p>
+                            <p className="skill">CSS</p>
+                            <p className="skill">JavaScript</p>
+                            <p className="skill">ReactJs</p>
+                            <p className="skill">NASA API</p>
+                        </div>
+                        <div className="project-links">
+                            <p className="demo">Live Demo</p>
+                            <p className="source">View Source</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img"></div>
+                    </div>
+                    <div className="center project-data">
+                        <h1>Wildfire Tracker</h1>
+                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
+                        <div className="project-skills">
+                            <p className="skill">HTML</p>
+                            <p className="skill">CSS</p>
+                            <p className="skill">JavaScript</p>
+                            <p className="skill">ReactJs</p>
+                            <p className="skill">NASA API</p>
+                        </div>
+                        <div className="project-links">
+                            <p className="demo">Live Demo</p>
+                            <p className="source">View Source</p>
+
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </div >
