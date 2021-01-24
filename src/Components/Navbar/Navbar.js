cca0208d56b3dab792a00ed8d737233e48a518ce
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-23 19:05:07 -0800

    updated header files and project files

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,10 @@ import './Navbar.css'
 const Navigation = () => {
     return (
         < div className="bar" >
-            <div className="nav">
-                <p> Github </p>
+            <div className="logo">
+                AB
             </div>
             <div className="nav">
-                <p> Linkedin </p>
-            </div>
-            <div className="nav">
-
                 <form action="/Anmol-Burmy.pdf" method="get" target="_blank">
                     <button className="btn" >Resume</button>
                 </form>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,15 +3,40 @@ import './Projects.css'
 
 const Projects = () => {
     return (
-        <div className="center container ">
-            <div className="center card ">
-                <div className="image">
-                    image here
+        <div className="center projects">
+            <div className="projects-box">
+
+                <h1 className="center mp">My Projects</h1>
+
+                <div className="center card ">
+                    <div className="center image">
+                        image here
+                     </div>
+                    <div className="center data">
+                        <h1>Wildfire Tracker</h1>
+                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
+                        <div className="skills">
+                            <p className="skill">HTML</p>
+                            <p className="skill">CSS</p>
+                            <p className="skill">JavaScript</p>
+                            <p className="skill">ReactJs</p>
+                            <p className="skill">NASA API</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="info">
-                    <h1 className="title">Wildlife tracker</h1>
-                    <p className="disc">loremndjbncdsobnuvs vcnisov </p>
+
+                <div className="center card ">
+                    <div className="image">
+                        image here
+                     </div>
                 </div>
+
+                <div className="center card ">
+                    <div className="image">
+                        image here
+                     </div>
+                </div>
+
             </div>
         </div >
     )
