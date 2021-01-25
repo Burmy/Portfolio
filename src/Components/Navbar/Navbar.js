848da473c82848d9100041831b88769636df6d7f
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-24 17:51:51 -0800

    added footer

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,6 +7,7 @@ const Navigation = () => {
             <div className="logo">
                 AB
             </div>
+
             <div className="nav">
                 <form action="/Anmol-Burmy.pdf" method="get" target="_blank">
                     <button className="btn" >Resume</button>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => {
                         <div className="img"></div>
                     </div>
                     <div className="center project-data">
-                        <h1>Wildfire Tracker</h1>
+                        <h1 className="name">Wildfire Tracker</h1>
                         <p>This website tracks the wildfires going around the world using a NASA API.</p>
                         <div className="project-skills">
                             <p className="skill">HTML</p>
@@ -23,101 +23,14 @@ const Projects = () => {
                             <p className="skill">NASA API</p>
                         </div>
                         <div className="project-links">
+
+
                             <p className="demo">Live Demo</p>
                             <p className="source">View Source</p>
 
                         </div>
                     </div>
                 </div>
-
-                <div className="center project-card ">
-                    <div className="center project-image">
-                        <div className="img"></div>
-                    </div>
-                    <div className="center project-data">
-                        <h1>Wildfire Tracker</h1>
-                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
-                        <div className="project-skills">
-                            <p className="skill">HTML</p>
-                            <p className="skill">CSS</p>
-                            <p className="skill">JavaScript</p>
-                            <p className="skill">ReactJs</p>
-                            <p className="skill">NASA API</p>
-                        </div>
-                        <div className="project-links">
-                            <p className="demo">Live Demo</p>
-                            <p className="source">View Source</p>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="center project-card ">
-                    <div className="center project-image">
-                        <div className="img"></div>
-                    </div>
-                    <div className="center project-data">
-                        <h1>Wildfire Tracker</h1>
-                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
-                        <div className="project-skills">
-                            <p className="skill">HTML</p>
-                            <p className="skill">CSS</p>
-                            <p className="skill">JavaScript</p>
-                            <p className="skill">ReactJs</p>
-                            <p className="skill">NASA API</p>
-                        </div>
-                        <div className="project-links">
-                            <p className="demo">Live Demo</p>
-                            <p className="source">View Source</p>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="center project-card ">
-                    <div className="center project-image">
-                        <div className="img"></div>
-                    </div>
-                    <div className="center project-data">
-                        <h1>Wildfire Tracker</h1>
-                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
-                        <div className="project-skills">
-                            <p className="skill">HTML</p>
-                            <p className="skill">CSS</p>
-                            <p className="skill">JavaScript</p>
-                            <p className="skill">ReactJs</p>
-                            <p className="skill">NASA API</p>
-                        </div>
-                        <div className="project-links">
-                            <p className="demo">Live Demo</p>
-                            <p className="source">View Source</p>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="center project-card ">
-                    <div className="center project-image">
-                        <div className="img"></div>
-                    </div>
-                    <div className="center project-data">
-                        <h1>Wildfire Tracker</h1>
-                        <p>This website tracks the wildfires going around the world using a NASA API.</p>
-                        <div className="project-skills">
-                            <p className="skill">HTML</p>
-                            <p className="skill">CSS</p>
-                            <p className="skill">JavaScript</p>
-                            <p className="skill">ReactJs</p>
-                            <p className="skill">NASA API</p>
-                        </div>
-                        <div className="project-links">
-                            <p className="demo">Live Demo</p>
-                            <p className="source">View Source</p>
-
-                        </div>
-                    </div>
-                </div>
-
 
             </div>
         </div >
