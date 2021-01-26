31864a2a0ce49e9b00daeae45d79587856b9a41f
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-26 14:10:01 -0800

    project images and info update

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -39,8 +39,8 @@ const Projects = () => {
                 <div className="center project-card ">
 
                     <div className="center project-data">
-                        <h1 className="name-opp">Wildfire Tracker</h1>
-                        <p className="info-opp">This website tracks the wildfires going around the world using a NASA API. </p>
+                        <h1 className="name-opp">Crypto Currency Tracker</h1>
+                        <p className="info-opp">A website that shows Crypto Currency trends. This was made during the time when crypto currency reached its all time high value.</p>
                         <ul className="project-skills-opp">
                             <li className="skill">HTML</li>
                             <li className="skill">CSS</li>
@@ -64,6 +64,61 @@ const Projects = () => {
                 </div>
 
 
+                <div className="center project-card ">
+                    <div className="center project-image">
+                        <div className="img3">
+                            <div className="project-links">
+
+                                <form action="https://burmy.github.io/Wildfire-tracker/" method="get" target="_blank">
+                                    <button className="demo">Live Demo</button>
+                                </form>
+                                <form action="https://github.com/Burmy/Wildfire-tracker" method="get" target="_blank">
+                                    <button className="source">View Source</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="center project-data">
+                        <h1 className="name">Face detecting AI</h1>
+                        <p className="info">A website that detects faces from images taken anywhere from the internet. This was a part of a Udemy course I took during the summer of 2020. </p>
+                        <ul className="project-skills">
+                            <li className="skill">HTML</li>
+                            <li className="skill">CSS</li>
+                            <li className="skill">JavaScript</li>
+                            <li className="skill">ReactJs</li>
+                            <li className="skill">NASA API</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+
+                <div className="center project-card ">
+
+                    <div className="center project-data">
+                        <h1 className="name-opp">Robo Friends</h1>
+                        <p className="info-opp">This app is just a simple user search app and was the first react app I ever created.</p>
+                        <ul className="project-skills-opp">
+                            <li className="skill">HTML</li>
+                            <li className="skill">CSS</li>
+                            <li className="skill">JavaScript</li>
+                            <li className="skill">ReactJs</li>
+                            <li className="skill">NASA API</li>
+                        </ul>
+                    </div>
+                    <div className="center project-image">
+                        <div className="img4">
+                            <div className="project-links">
+                                <form action="https://burmy.github.io/Wildfire-tracker/" method="get" target="_blank">
+                                    <button className="demo">Live Demo</button>
+                                </form>
+                                <form action="https://github.com/Burmy/Wildfire-tracker" method="get" target="_blank">
+                                    <button className="source">View Source</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div >
     )
