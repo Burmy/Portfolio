d06fcf5378e839c5af1b07c57e91304597562b8e
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-26 17:37:50 -0800

    updated project links

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,27 +11,29 @@ const Projects = () => {
                 <div className="center project-card ">
                     <div className="center project-image">
                         <div className="img1">
-                            <div className="project-links">
 
-                                <form action="https://burmy.github.io/Wildfire-tracker/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
-                                </form>
-                                <form action="https://github.com/Burmy/Wildfire-tracker" method="get" target="_blank">
-                                    <button className="source">View Source</button>
-                                </form>
-                            </div>
                         </div>
                     </div>
                     <div className="center project-data">
                         <h1 className="name">Wildfire Tracker</h1>
                         <p className="info">This website tracks the wildfires going around the world using a NASA API. </p>
+                        <div className="project-links">
+
+                            <form action="https://burmy.github.io/Wildfire-tracker/" method="get" target="_blank">
+                                <button className="demo">Live Demo</button>
+                            </form>
+                            <form action="https://github.com/Burmy/Wildfire-tracker" method="get" target="_blank">
+                                <button className="source">View Source</button>
+                            </form>
+                        </div>
                         <ul className="project-skills">
                             <li className="skill">HTML</li>
                             <li className="skill">CSS</li>
                             <li className="skill">JavaScript</li>
-                            <li className="skill">ReactJs</li>
+                            <li className="skill">React</li>
                             <li className="skill">NASA API</li>
                         </ul>
+
                     </div>
                 </div>
 
@@ -41,24 +43,24 @@ const Projects = () => {
                     <div className="center project-data">
                         <h1 className="name-opp">Crypto Currency Tracker</h1>
                         <p className="info-opp">A website that shows Crypto Currency trends. This was made during the time when crypto currency reached its all time high value.</p>
+                        <div className="project-links">
+                            <form action="https://burmy.github.io/Crypto-tracker/" method="get" target="_blank">
+                                <button className="demo">Live Demo</button>
+                            </form>
+                            <form action="https://github.com/Burmy/Crypto-tracker" method="get" target="_blank">
+                                <button className="source">View Source</button>
+                            </form>
+                        </div>
                         <ul className="project-skills-opp">
                             <li className="skill">HTML</li>
                             <li className="skill">CSS</li>
                             <li className="skill">JavaScript</li>
-                            <li className="skill">ReactJs</li>
-                            <li className="skill">NASA API</li>
+                            <li className="skill">React</li>
                         </ul>
                     </div>
                     <div className="center project-image">
                         <div className="img2">
-                            <div className="project-links">
-                                <form action="https://burmy.github.io/Crypto-tracker/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
-                                </form>
-                                <form action="https://github.com/Burmy/Crypto-tracker" method="get" target="_blank">
-                                    <button className="source">View Source</button>
-                                </form>
-                            </div>
+
                         </div>
                     </div>
                 </div>
@@ -67,26 +69,24 @@ const Projects = () => {
                 <div className="center project-card ">
                     <div className="center project-image">
                         <div className="img3">
-                            <div className="project-links">
-
-                                <form action="https://face-detecting-ai.herokuapp.com/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
-                                </form>
-                                <form action="https://github.com/Burmy/face-ai" method="get" target="_blank">
-                                    <button className="source">View Source</button>
-                                </form>
-                            </div>
                         </div>
                     </div>
                     <div className="center project-data">
-                        <h1 className="name">Face detecting AI</h1>
+                        <h1 className="name">Face Detecting AI</h1>
                         <p className="info">A website that detects faces from images taken anywhere from the internet. This was a part of a Udemy course I took during the summer of 2020. </p>
+                        <div className="project-links">
+                            <form action="https://face-detecting-ai.herokuapp.com/" method="get" target="_blank">
+                                <button className="demo">Live Demo</button>
+                            </form>
+                            <form action="https://github.com/Burmy/face-ai" method="get" target="_blank">
+                                <button className="source">View Source</button>
+                            </form>
+                        </div>
                         <ul className="project-skills">
                             <li className="skill">HTML</li>
                             <li className="skill">CSS</li>
                             <li className="skill">JavaScript</li>
                             <li className="skill">ReactJs</li>
-                            <li className="skill">NASA API</li>
                         </ul>
                     </div>
                 </div>
@@ -98,24 +98,24 @@ const Projects = () => {
                     <div className="center project-data">
                         <h1 className="name-opp">Robo Friends</h1>
                         <p className="info-opp">This app is just a simple user search app and was the first react app I ever created.</p>
+                        <div className="project-links">
+                            <form action="https://burmy.github.io/robo-friends/" method="get" target="_blank">
+                                <button className="demo">Live Demo</button>
+                            </form>
+                            <form action="https://github.com/Burmy/robo-friends" method="get" target="_blank">
+                                <button className="source">View Source</button>
+                            </form>
+                        </div>
                         <ul className="project-skills-opp">
                             <li className="skill">HTML</li>
                             <li className="skill">CSS</li>
                             <li className="skill">JavaScript</li>
                             <li className="skill">ReactJs</li>
-                            <li className="skill">NASA API</li>
                         </ul>
                     </div>
                     <div className="center project-image">
                         <div className="img4">
-                            <div className="project-links">
-                                <form action="https://burmy.github.io/robo-friends/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
-                                </form>
-                                <form action="https://github.com/Burmy/robo-friends" method="get" target="_blank">
-                                    <button className="source">View Source</button>
-                                </form>
-                            </div>
+
                         </div>
                     </div>
                 </div>
