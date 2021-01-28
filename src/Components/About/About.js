665ead8f16e87b23f31899cb0e94cd3be052a26d
Burmy <62005549+Burmy@users.noreply.github.com>
2021-01-27 17:21:22 -0800

    Footer fixes

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,8 +6,7 @@ const About = () => {
     return (
         <div className="center about">
             <div className="about-box">
-                {/* <h1>About Me</h1> */}
-                <p>A Computer Science student at San Fransisco State University who is passionate about becoming a Web Developer. I love creating things on the web Fast learner with a passion to serve for society and enjoys the challenge of identifying and solving problems. Very detailed oriented and self motivated team player who enjoys socializing with others.
+                <p>A Computer Science student at <span className="hl">San Fransisco State University</span> who is passionate about becoming a Web Developer. I love creating things on the web and be <span className="hl">very detailed</span> with them. I aspire toward a career that will allow me to channel my creativity through crafting <span className="hl">beautiful</span> software and <span className="hl">engaging</span> experiences.
                 </p>
             </div>
         </div>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 
 const Footer = () => {
     return (
         <div className="center footer">
             <div className="footer-box">
-                <p>Top of the page</p>
-                <a href="#top">
+                <AnchorLink href='#top'>
                     <btn className="arrow-up">
                         <span className="left-arm"></span>
                         <span className="right-arm"></span>
                         <span className="arrow-slide"></span>
-
+                        <p className="top center">Go To Top</p>
                     </btn>
-                </a>
+                </AnchorLink>
             </div>
         </div >
     )
