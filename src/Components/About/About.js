8ae49374679963fc36b2ff56021b1778e7b6e107
Burmy <62005549+Burmy@users.noreply.github.com>
2021-03-17 19:48:45 -0700

    updated about box

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,12 @@ import './About.css'
 
 const About = () => {
     return (
-        <div className="center about" id="about">
+        <div className="center about-container" id="About">
+            <div className="about-pngs">
+                <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fa25157d7c72d1dea0b55e4_fresh-1-top.png" alt="" className="aboutBG-1"></img>
+                <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fa2515700561360a501a17f_fresh-1-right.png" alt="" className="aboutBG-2"></img>
+                <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fa251575a35c45b3593c443_fresh-1-dots.png" alt="" className="aboutBG-3"></img>
+            </div>
             <div className="about-box">
                 <p>A Computer Science student at <span className="hl">San Fransisco State University</span> who is passionate about becoming a Web Developer. I love creating things on the web and be <span className="hl">very detailed</span> with them. I aspire toward a career that will allow me to channel my creativity through crafting <span className="hl">beautiful</span> software and <span className="hl">engaging</span> experiences.
                 </p>
