d235c10653c8031919a8a47c284a6a5f66560397
Burmy <62005549+Burmy@users.noreply.github.com>
2021-03-19 18:40:58 -0700

    updated project info layout

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,11 +4,11 @@ import Fade from 'react-reveal/Fade';
 
 const Projects = () => {
     return (
-        <div className="center projects" id="projects">
+        <div className="center projects" >
             <div className="project-box">
 
                 <Fade>
-                    <h1 className="center mp">My Projects</h1>
+                    <h1 className="center mp " id="projects">My Projects</h1>
                 </Fade>
 
                 <Fade>
@@ -22,6 +22,13 @@ const Projects = () => {
                             {/* <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fae400b820032ad1a780595_fresh-1-timeboss.png" alt="" class="porjectBG"></img> */}
                             <h1 className="name">Wildfire Tracker</h1>
                             <p className="info">This website tracks the wildfires going around the world using a NASA API. </p>
+                            <ul className="project-skills">
+                                <li className="skill">HTML</li>
+                                <li className="skill">CSS</li>
+                                <li className="skill">JavaScript</li>
+                                <li className="skill">React</li>
+                                <li className="skill">NASA API</li>
+                            </ul>
                             <div className="project-links">
                                 <form action="https://burmy.github.io/Wildfire-Tracker/" method="get" target="_blank">
                                     <button className="demo">Live Demo</button>
@@ -30,13 +37,7 @@ const Projects = () => {
                                     <button className="demo">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                                <li className="skill">NASA API</li>
-                            </ul>
+
                         </div>
                     </div>
                 </Fade>
@@ -47,20 +48,21 @@ const Projects = () => {
                         <div className="center project-data">
                             <h1 className="name-opp">SF Hacks</h1>
                             <p className="info-opp">This SF Hacks website I redesigned during SF Hacks 2021 and won a Web Competition for the best redesign.</p>
+                            <ul className="project-skills-opp">
+                                <li className="skill-opp">HTML</li>
+                                <li className="skill-opp">CSS</li>
+                                <li className="skill-opp">JavaScript</li>
+                                <li className="skill-opp">React</li>
+                            </ul>
                             <div className="project-links-opp">
                                 <form action="https://burmy.github.io/sfhacks/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
+                                    <button className="demo-opp">Live Demo</button>
                                 </form>
                                 <form action="https://github.com/Burmy/sfhacks" method="get" target="_blank">
-                                    <button className="demo">View Source</button>
+                                    <button className="demo-opp">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills-opp">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                            </ul>
+
                         </div>
                         <div className="center project-image">
                             <div className="img-hacks">
@@ -79,6 +81,12 @@ const Projects = () => {
                             {/* <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fae400b820032ad1a780595_fresh-1-timeboss.png" alt="" class="porjectBG"></img> */}
                             <h1 className="name">Crypto Currency Tracker</h1>
                             <p className="info">A website that shows Crypto Currency trends. This was made during the time when crypto currency reached its all time high value.</p>
+                            <ul className="project-skills">
+                                <li className="skill">HTML</li>
+                                <li className="skill">CSS</li>
+                                <li className="skill">JavaScript</li>
+                                <li className="skill">React</li>
+                            </ul>
                             <div className="project-links">
                                 <form action="https://burmy.github.io/Crypto-Tracker/" method="get" target="_blank">
                                     <button className="demo">Live Demo</button>
@@ -87,12 +95,7 @@ const Projects = () => {
                                     <button className="demo">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                            </ul>
+
                         </div>
 
                     </div>
@@ -106,20 +109,21 @@ const Projects = () => {
                             {/* <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fae402787959bb2ce47e195_fresh-1-spreadsheet.png" alt="" class="porjectBG-opp"></img> */}
                             <h1 className="name-opp">My Portfolio</h1>
                             <p className="info-opp">A website showcasing all my projects.</p>
+                            <ul className="project-skills-opp">
+                                <li className="skill-opp">HTML</li>
+                                <li className="skill-opp">CSS</li>
+                                <li className="skill-opp">JavaScript</li>
+                                <li className="skill-opp">React</li>
+                            </ul>
                             <div className="project-links-opp">
                                 <form action="https://burmy.me/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
+                                    <button className="demo-opp">Live Demo</button>
                                 </form>
                                 <form action="https://github.com/Burmy/portfolio" method="get" target="_blank">
-                                    <button className="demo">View Source</button>
+                                    <button className="demo-opp">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills-opp">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                            </ul>
+
                         </div>
                         <div className="center project-image">
                             <div className="img-portfolio">
@@ -139,6 +143,12 @@ const Projects = () => {
                             {/* <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fae400b820032ad1a780595_fresh-1-timeboss.png" alt="" class="porjectBG"></img> */}
                             <h1 className="name">Face Detecting AI</h1>
                             <p className="info">A website that detects faces from images taken anywhere from the internet. This was a part of a Udemy course I took during the summer of 2020. </p>
+                            <ul className="project-skills">
+                                <li className="skill">HTML</li>
+                                <li className="skill">CSS</li>
+                                <li className="skill">JavaScript</li>
+                                <li className="skill">React</li>
+                            </ul>
                             <div className="project-links">
                                 <form action="https://face-detecting-ai.herokuapp.com/" method="get" target="_blank">
                                     <button className="demo">Live Demo</button>
@@ -147,12 +157,6 @@ const Projects = () => {
                                     <button className="demo">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                            </ul>
                         </div>
 
                     </div>
@@ -165,20 +169,20 @@ const Projects = () => {
                             {/* <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fae402787959bb2ce47e195_fresh-1-spreadsheet.png" alt="" class="porjectBG-opp"></img> */}
                             <h1 className="name-opp">Robo Friends</h1>
                             <p className="info-opp">This app is just a simple user search app and was the first react app I ever created.</p>
+                            <ul className="project-skills-opp">
+                                <li className="skill-opp">HTML</li>
+                                <li className="skill-opp">CSS</li>
+                                <li className="skill-opp">JavaScript</li>
+                                <li className="skill-opp">React</li>
+                            </ul>
                             <div className="project-links-opp">
                                 <form action="https://burmy.github.io/robo-friends/" method="get" target="_blank">
-                                    <button className="demo">Live Demo</button>
+                                    <button className="demo-opp">Live Demo</button>
                                 </form>
                                 <form action="https://github.com/Burmy/robo-friends" method="get" target="_blank">
-                                    <button className="demo">View Source</button>
+                                    <button className="demo-opp">View Source</button>
                                 </form>
                             </div>
-                            <ul className="project-skills-opp">
-                                <li className="skill">HTML</li>
-                                <li className="skill">CSS</li>
-                                <li className="skill">JavaScript</li>
-                                <li className="skill">React</li>
-                            </ul>
                         </div>
                         <div className="center project-image">
                             <div className="img-robo">
