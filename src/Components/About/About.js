fc5e2c72159309cca3e6255fa6a0e32112bd003b
Burmy <62005549+Burmy@users.noreply.github.com>
2021-04-12 19:03:54 -0700

    minor updates

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 <img src="https://assets.website-files.com/5d7fff8297d2f2362bc02ca9/5fa251575a35c45b3593c443_fresh-1-dots.png" alt="" className="aboutBG-3"></img>
             </div>
             <div className="about-box">
-                <p>A Computer Science student at <span className="hl">San Fransisco State University</span> who is passionate about becoming a Web Developer. I love creating things on the web and be <span className="hl">very detailed</span> with them. I aspire toward a career that will allow me to channel my creativity through crafting <span className="hl">beautiful</span> software and <span className="hl">engaging</span> experiences.
+                <p>A Computer Science student at <span className="hl">San Francisco State University</span> who is passionate about becoming a Web Developer. I love creating things on the web and be <span className="hl">very detailed</span> with them. I aspire toward a career that will allow me to channel my creativity through crafting <span className="hl">beautiful</span> software and <span className="hl">engaging</span> experiences.
                 </p>
             </div>
         </div>
