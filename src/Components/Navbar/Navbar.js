c2d644c4017c4c4ae28cae32273f5bfff2831e22
Anmol Burmy <anmolburmy145@gmail.com>
2022-12-06 14:29:21 -0600

    refactoring stuff

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,36 +1,31 @@
-import React from 'react';
-import './Navbar.css'
-import PDF from '../Navbar/Resume.pdf';
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import "./Navbar.css";
+import PDF from "../Navbar/Resume.pdf";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navigation = () => {
-    return (
-        < div className="bar" id="Navbar" >
-            <div className="logo">
-            </div>
+  return (
+    <div className="bar" id="Navbar">
+      <div className="logo"></div>
 
-            <nav className="nav">
-                <ul className="nav-ul">
-
-                    {/* <AnchorLink href='#About'>
+      <nav className="nav">
+        <ul className="nav-ul">
+          {/* <AnchorLink href='#About'>
                         <li>About</li>
                     </AnchorLink> */}
 
-                    <AnchorLink className="projects" href='#projects'>
-                        <li>Projects</li>
-                    </AnchorLink>
-
-                </ul>
-                <a href={PDF} without rel="noopener noreferrer" target="_blank">
-                    <button className="btn" trailingIcon="picture_as_pdf" label="Resume">
-                        Resume
-                    </button>
-                </a>
-            </nav>
-
-        </ div >
-
-    );
-}
+          <AnchorLink className="projects" href="#projects">
+            <li>Projects</li>
+          </AnchorLink>
+        </ul>
+        <a href={PDF} without rel="noopener noreferrer" target="_blank">
+          <button className="btn" trailingIcon="picture_as_pdf" label="Resume">
+            Resume
+          </button>
+        </a>
+      </nav>
+    </div>
+  );
+};
 
 export default Navigation;
